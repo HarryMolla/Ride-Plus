d83e493c8472dae601e3906c6ffc095f06eaeb66
HarryMolla <hmiol5577@gmail.com>
2025-09-30 01:41:21 +0300

    learn03 added

--- src/app/Learn03.tsx
+++ src/app/Learn03.tsx
@@ -29,44 +29,37 @@ interface TimelineStep {
 const steps: TimelineStep[] = [
   {
     number: 1,
-    title: "Purchase Credit",
+    title: "Loyalty Points will be rewarded after every trip.",
     description:
-      "As a driver, you can buy credit directly from your bank or mobile money service, ensuring you always have funds available.",
+      "",
   },
   {
     number: 2,
-    title: "Earn and Exchange",
+    title: "Navigate to the Ride Points page.",
     description:
-      "You can also exchange your earned balance for credit, providing flexibility in managing your earnings.",
+      "",
   },
   {
     number: 3,
-    title: "Automatic Credit Management: ",
+    title: "Choose among the available redeem options and enjoy.",
     description:
-      "The app will automatically update your credit balance after each transaction, so you always know where you stand.",
-  },
-  {
-    number: 4,
-    title: "Stay Prepared",
-    description:
-      "You can purchase additional credit anytime to ensure you're always ready for more pickups.",
+      "",
   },
 ];
 
-const Learn01 = () => {
+const Learn03 = () => {
   return (
-    <div className="relative flex flex-col items-start gap-4 py-0 px-5 md:max-w-10/12 mx-auto md:h-screen my-20">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 justify-items-center">
-        <div className="order-2 md:order-1 grid gap-4">
+    <div className="relative flex flex-col items-start gap-4 py-0 px-5 md:max-w-10/12 mx-auto md:h-fit mb-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+        <div className="order-2 md:order-2 grid gap-4 h-fit">
           <div className="grid gap-2">
             <h1 className="text-gray-300 font-bold md:text-3xl text-2xl ">
-              Flexible Credit Purchase (For Drivers)
+              Earn Rewards for Every Ride (For Drivers and Passengers)
             </h1>
             <p className="text-gray-400">
-              With Flexible Credit Purchase, drivers can effortlessly buy credit
-              from their favorite bank or mobile money services. They can also
-              exchange credit from their earnings, making it easier to keep
-              earning while managing their finances.
+              Passengers and drivers can now enjoy our Loyalty Points program!
+              Accumulate points with every trip and redeem them for exciting
+              rewards.
             </p>
           </div>
           <h1 className="text-gray-100 text-lg font-bold">How it Works</h1>
@@ -77,7 +70,7 @@ const Learn01 = () => {
               className="
                       absolute left-[1.4rem] w-1 bg-white/10 rounded 
                       top-[1.5rem] 
-                      h-[calc(100%-8.4rem)] md:h-[calc(100%-9rem)] lg:h-[calc(100%-8rem)]"
+                      h-[calc(100%-6.4rem)] md:h-[calc(100%-8rem)] lg:h-[calc(100%-6rem)]"
             >
               <div className="absolute w-full h-8 bg-gradient-to-t from-white to-transparent animate-gradientMove rounded-b-full"></div>
             </div>
@@ -85,22 +78,28 @@ const Learn01 = () => {
             {steps.map((step, idx) => (
               <div
                 key={idx}
-                className="grid grid-cols-[48px_1fr] items-start gap-4 relative mb-8"
+                className="grid grid-cols-[48px_1fr] items-center gap-4 relative mb-8"
               >
                 <Circle className="bg-gray-300">{step.number}</Circle>
                 <div>
                   <h1 className="text-gray-300 text-lg font-bold">
                     {step.title}
                   </h1>
-                  <p className="text-gray-400">{step.description}</p>
+                  
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="order-1 md:order-2">
-          <video className="md:h-140 object-cover" autoPlay loop muted playsInline>
-            <source src="./learn01.webm" type="video/webm" />
+        <div className="order-1 md:order-1">
+          <video
+            className="md:h-140 object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src="./learn03.webm" type="video/webm" />
             Your browser does not support the video tag.
           </video>
         </div>
@@ -109,4 +108,4 @@ const Learn01 = () => {
   );
 };
 
-export default Learn01;
+export default Learn03;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import OurCustomer from '@/OurCustomer'
-import Learn from '@/app/Learn01'
+import Learn01 from '@/app/Learn01'
+import Learn03 from './Learn03'
 
 const page = () => {
   return (
     <div className='grid grid-cols-1 justify-items-center bg-black'>
     <Hero/>
     <OurCustomer/>
-    <Learn/>
+    <Learn01/>
+    <Learn03/>
     </div>
   )
 }
